bc7fd1ffce101ff1ee5afa18159200c11acd8244
Cameron Laury <CameronL@cpe-172-100-4-178.twcny.res.rr.com>
2017-02-14 12:47:00 -0600

    another update

--- MemeMe_V1/SentMemesTableViewController.swift
+++ MemeMe_V1/SentMemesTableViewController.swift
@@ -44,7 +44,7 @@ class SentMemesTableViewController: UITableViewController {
     // Here it is! -----
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "MemeTableViewCell") as! MemeTableViewCell
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "sentMemesTableView") as! MemeTableViewCell
         let meme = memes[indexPath.row]
         
         tableViewCell.tableViewImage.image = meme.memedImage
@@ -58,7 +58,7 @@ class SentMemesTableViewController: UITableViewController {
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return false
+        return true
     }
     
     // Override to support conditional rearranging of the table view.
@@ -67,11 +67,9 @@ class SentMemesTableViewController: UITableViewController {
         return false
     }
 
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func deleteMemesInTableViewCell(_ index: Int) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memes.remove(at: index)
     }
+}
 
